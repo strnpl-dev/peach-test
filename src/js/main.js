b1ae360f97aee729be1fc117b57a047fcafcc3b0
Robert <strnpl.dev@gmail.com>
2024-04-17 20:36:40 +0300

    added dots to the map

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,24 @@ import slider from './modules/slider'
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    var sections = document.querySelectorAll('.section');
 
+    window.addEventListener('scroll', function () {
+        sections.forEach(function (section) {
+            if (isElementInViewport(section)) {
+                section.classList.add('animate__animated', 'animate__slideInUp');
+            }
+        });
+    });
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
 })
